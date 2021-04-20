5a774ae7379f4f1f309b910ecbe835e5bccb2747
Nuria A. Malet Quintar <nmaletq.pr@gmail.com>
2021-04-20 17:11:45 -0300

    Added create new post functionality

--- individual.challenge/src/main/java/org/alkemy/java/individual/challenge/main/controller/PostController.java
+++ individual.challenge/src/main/java/org/alkemy/java/individual/challenge/main/controller/PostController.java
@@ -1,18 +1,21 @@
 package org.alkemy.java.individual.challenge.main.controller;
 
 import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
+import javax.validation.Valid;
 import org.alkemy.java.individual.challenge.main.model.Post;
 import org.alkemy.java.individual.challenge.main.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
 @SessionAttributes("post")
@@ -39,6 +42,23 @@ public class PostController  {
 		}
 		model.addAttribute("post", post);
 		return "post-view";
+	}
+	
+    @GetMapping("/addPost")
+    public String addPostForm(Model model) {
+        Post post = new Post();
+        model.addAttribute("post", post);
+        return "add-post-form";
+    }
+
+	@PostMapping(value="/posts")
+    public String addCourse(@Valid @ModelAttribute Post post, BindingResult bindingResult, SessionStatus status) {	 
+		if(bindingResult.hasErrors()) {	    
+			return "add-post-form";
+		}
+		postService.save(post);
+		status.isComplete();
+        return "redirect:/home";   
 	}
 
 	
